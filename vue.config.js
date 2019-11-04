163a6990571c4a439886fdc84c2f0a81b28f9f63
jieruian <jieruian@163.com>
2019-11-04 18:04:40 +0800

    完善stylus

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+const path = require('path')
 module.exports = {
+  //  chainWebpack: config => {
+  //    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+  //    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
+  //  },
     configureWebpack: {
         resolve: {
             alias: {
@@ -14,7 +19,25 @@ module.exports = {
     pluginOptions: {
       'style-resources-loader': {
         preProcessor: 'stylus',
-        patterns: []
+        patterns: [
+          //  path.resolve(__dirname, './src/assets/style/variable.styl'),
+          //   path.resolve(__dirname, './src/assets/style/reset.styl'),
+          //   path.resolve(__dirname, './src/assets/style/base.styl'),
+          //   path.resolve(__dirname, './src/assets/style/icon.styl'),
+        ]
       }
     }
+}
+
+function addStyleResource(rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/style/variable.styl'),
+        path.resolve(__dirname, './src/assets/style/reset.styl'),
+        path.resolve(__dirname, './src/assets/style/base.styl'),
+        path.resolve(__dirname, './src/assets/style/icon.styl'),
+      ],
+    })
 }
