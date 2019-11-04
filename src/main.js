e53f09ef96983328198e3a59b9397adaab049019
jieruian <jieruian@163.com>
2019-11-04 11:48:08 +0800

    配置基础文件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueLazyLoad from 'vue-lazyload'
+import router from './router'
+import store from './store'
+
+import FastClick from 'fastclick'
 
 Vue.config.productionTip = false
+//解决移动端300ms延迟
+FastClick.attach(document.body)
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    base: '/',
+    mode: 'history',
+    routes: []
+});
